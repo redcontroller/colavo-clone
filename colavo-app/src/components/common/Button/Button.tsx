ca6fd475b370c1e98a06778864cb166c0ed12e65
redcontroller <jasinis102@gmail.com>
2024-06-09 18:55:58 +0900

    :sparkles: feat: 헤더 컴포넌트 개발(1)

--- colavo-app/src/components/common/Button/Button.tsx
+++ colavo-app/src/components/common/Button/Button.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
-import { StyledImg, StyledButton, StyledSpan } from './ButtonStyle';
-import icon_Add from '../../assets/icon/icon_add.svg';
+import { 
+    StyledIcon,
+    StyledImg,
+    StyledButton,
+    StyledSpan
+} from './ButtonStyle';
 
 interface ButtonOptions {
     text?: string;
     color?: 'blue' | 'gray' | 'darkGray';
     disabled?: boolean;
-    type?: 'default' | 'mixed' | 'image';
+    type?: 'default' | 'mixed' | 'image' | 'icon';
+    iconPath?: string;
     onClick?: (even: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-const Button = ({ text, color, disabled=false, type='default', onClick }: ButtonOptions) => {
+const Button = ({ text, color, disabled=false, type='default', iconPath, onClick }: ButtonOptions) => {
 
     return (
         <>
             { type==='image' ? (
-                    <StyledImg src={icon_Add} alt='할인 메뉴 추가' />
+                    <StyledImg src={iconPath} alt='할인 메뉴 추가' />
+                ): type==='icon' ? (
+                    <StyledIcon src={iconPath} alt='할인 메뉴 추가' />
                 ):(
                     <StyledButton
                         className={`button_${color}`}
--- colavo-app/src/pages/Cart.tsx
+++ colavo-app/src/pages/Cart.tsx
@@ -1,10 +1,67 @@
-import React from 'react';
-import Button from '../components/common/Button';
+// import { useParams, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import Button from '../components/common/Button/Button';
+import Header, { UserInfo } from '../components/common/Header/Header';
+import formatTime from '../feature/formatTime';
+import icon_add from '../assets/icon/icon_add.svg';
+import icon_esc from '../assets/icon/icon_esc.svg';
+import icon_back from '../assets/icon/icon_back.svg';
 
 const Cart = () => {
+    // const params = useParams();
+    const nav = useNavigate();
+
+    const info:UserInfo = {
+        name: '김모건',
+        manager: '콜라보디자이너',
+        reserved: formatTime(new Date(), 'ko-KR'),
+        // reserved: formatTime(now, 'en-US'),
+    };
+
     return (
         <div>
             Shopping Cart Page
+            <Header
+                leftChild={
+                    <Button
+                        type='icon'
+                        iconPath={icon_esc}
+                        onClick={() => nav(-1)}
+                    />
+                }
+                info={info}
+            />
+            <Header
+                leftChild={
+                    <Button
+                        type='icon'
+                        iconPath={icon_back}
+                        onClick={() => nav(-1)}
+                    />
+                }
+                info={info}
+                text='시술 메뉴'
+            />
+            <Header
+                leftChild={
+                    <Button
+                        type='icon'
+                        iconPath={icon_back}
+                        onClick={() => nav(-1)}
+                    />
+                }
+                text='할인 메뉴'
+                rightChild={
+                    <Button
+                        type='icon'
+                        iconPath={icon_back}
+                        onClick={() => alert("준비 중 입니다.")}
+                    />
+                }
+            />
+
+
+
             <div>
                 <Button 
                     text='다음'
@@ -36,6 +93,7 @@ const Cart = () => {
             <div>
                 <Button 
                     type='image'
+                    iconPath={icon_add}
                 />
             </div>
             <div>
